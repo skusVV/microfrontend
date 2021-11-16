8be4cdcc2c748b87c58c6f655789d6c86d679fc7
skusVV <skus1992@gmail.com>
2021-11-16 20:35:49 +0200

    initial commit

---
+++ packages/marketing/src/bootstrap.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './app';
+
+const mount = el => {
+    ReactDOM.render(
+        <App />,
+        el
+    );
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const element = document.querySelector('._marketing-dev-root');
+
+    if (element) {
+        mount(element)
+    }
+}
+
+export { mount };
