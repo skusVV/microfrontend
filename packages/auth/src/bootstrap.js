8231db3a049839089f52f0b883892aa1cc39da20
skusVV <skus1992@gmail.com>
2021-11-17 23:38:24 +0200

    working with auth

---
+++ packages/auth/src/bootstrap.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { createMemoryHistory, createBrowserHistory } from 'history';
+import App from './app';
+
+
+const mount = (el, { onNavigate, defaultHistory }) => {
+    const history = defaultHistory ? defaultHistory : createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
+    ReactDOM.render(
+        <App history={history}/>,
+        el
+    );
+
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const element = document.querySelector('._auth-dev-root');
+
+    if (element) {
+        mount(element, { defaultHistory: createBrowserHistory() });
+    }
+}
+
+export { mount };
--- packages/container/src/app.js
+++ packages/container/src/app.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
 import MarketingApp from './components/MarketingApp';
+import AuthApp from './components/AuthApp';
 import Header from './components/Header';
 
 const generateClassName = createGenerateClassName({ productionPrefix: 'co' });
@@ -12,7 +13,11 @@ export default () => {
             <StylesProvider generateClassName={generateClassName}>
                 <div>
                     <Header />
-                    <MarketingApp />
+                    <Switch>
+                        <Route path="/auth" component={AuthApp}/>
+                        <Route path="/" component={MarketingApp}/>
+                        <Route />
+                    </Switch>
                 </div>
             </StylesProvider>
         </BrowserRouter>
