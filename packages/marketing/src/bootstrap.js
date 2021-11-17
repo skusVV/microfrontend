dd5e94444b4a6713a69c602da2e41a9d991dbb1c
skusVV <skus1992@gmail.com>
2021-11-17 21:14:55 +0200

    added navigation comunication container - marketing

--- packages/marketing/src/bootstrap.js
+++ packages/marketing/src/bootstrap.js
@@ -1,19 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createMemoryHistory } from 'history';
 import App from './app';
 
-const mount = el => {
+
+const mount = (el, { onNavigate }) => {
+    const history = createMemoryHistory();
+
+    if (onNavigate) {
+        history.listen(onNavigate);
+    }
+
     ReactDOM.render(
-        <App />,
+        <App history={history}/>,
         el
     );
+
+    return {
+        onParentNavigate: ({ pathname: nextPathname }) => {
+            const { pathname } = history.location;
+
+            if (pathname !== nextPathname) {
+                history.push(nextPathname);
+            }
+        }
+    }
 };
 
 if (process.env.NODE_ENV === 'development') {
     const element = document.querySelector('._marketing-dev-root');
 
     if (element) {
-        mount(element)
+        mount(element, {});
     }
 }
 
