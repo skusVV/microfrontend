a857b8a0177229c3ae06a0d800c5f4ba246a5534
skusVV <skus1992@gmail.com>
2021-11-18 23:18:17 +0200

    added dashboard

---
+++ packages/dashboard/src/bootstrap.js
@@ -0,0 +1,18 @@
+import { createApp} from 'vue';
+import Dashboard from './components/Dashboard';
+
+const mount = (el) => {
+    const app = createApp(Dashboard);
+
+    app.mount(el);
+};
+
+if (process.env.NODE_ENV === 'development') {
+    const element = document.querySelector('._dashboard-dev-root');
+
+    if (element) {
+        mount(element);
+    }
+}
+
+export { mount };
